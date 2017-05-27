c8d73b4db3d010fd61ae1c880ae17cf9b4a76e78
siorekoskar <siorek.oskar1@gmail.com>
2017-05-27 22:01:55 +0200

    Update project.ino

--- project.ino
+++ project.ino
@@ -1,29 +1,37 @@
-#define A 10
-#define B 11
-#define C 12
-#define D 13
-#define E 14
-#define F 15
-#define G 16
-#define DOT 13
+#define MAXDIGITS 3
 
-int d1=2,d2=3,d3=4,d4=5;
+int dark = 10;
+int medium = 100;
+int high = 255;
+
+
+int numberRight;
+byte digits[4] = {3,5,6,9};
+byte pins[8] = {2,4,7,8,10,11,12,13};
 const byte numChars = 32;
 char receivedChars[numChars]; 
 boolean newData = false;
 boolean programStarted = false;
+int speedt = 4500;
+int brightness =255;
+int brightns[4] = {255,255,255,255};
+int nums[6] = {0,1,2,3,4,5};
+
+byte string[6];
 
 void writeNumber(int whichNumber);
 void setupBars();
 void setupDigits();
+void stopDigit(int num);
 void deleteNumber();
 void activateDigitWithNumber();
+void readSpeed();
 
 void showNewData();
 boolean recvWithEndMarker();
 int parseData();
 
-byte arr[16] = {
+const int numTable[16] = {
     B11111100, //0
     B01100000, //1
     B11011010, //2
@@ -42,49 +50,102 @@ byte arr[16] = {
     B10001110 //F
 };
 
-
 ////////////////////////////////////////SETUP/////////////////////////////////
 void setup() {
   Serial.begin(9600);
   Serial.println("<Arduino is ready>");
   setupDigits();
   setupBars();
+  
 }
 
 ////////////////////////////////////////MAIN LOOP/////////////////////////////////
+
 void loop() {
-  if(programStarted){
-    for(int i = 4; i>=1; i--){
-      digitChanger(i);
-      activateDigitWithNumber();
-      delay(400);
-    }
   
-   } else{
-      activateDigitWithNumber();
-   }
+    
+  
+  numberWriter(nums);
+
 }
 
-void digitChanger(int digitToStart){
-    startDigit(digitToStart);
-    if(digitToStart == 4){
-      stopDigit(1);
-    } else {
-      stopDigit(digitToStart+1);
-    }
+void writeOnDigit(int number, int digit){
+  analogWrite(digits[digit], 250);
+    writeNumber(number);
 }
 
-void activateDigitWithNumber(){
-  if(recvWithEndMarker()){
-    showNewData();
-    int parsed = parseData();
-    deleteNumber();
-    writeNumber(parsed);
-    if(!programStarted){
-      startProgram();
+////////////////////////////////////////FUNCTIONS/////////////////////////////////
+
+void numberWriter(int nums[6]){
+
+  for(int i =10; i>=0; i--){
+   for(int j =0 ; j<=10; j++){
+    startDigit(i);
+    stopDigit(i-1);
+    stopDigit(i-2);
+    stopDigit(i-3);
+    stopDigit(i-4);
+  stopDigit(i-5);
+    writeNumber(nums[5]);
+  
+  delayMicroseconds(speedt);
+
+  stopDigit(i);
+  startDigit(i-1);
+  stopDigit(i-2);
+  stopDigit(i-3);
+  stopDigit(i-4);
+  stopDigit(i-5);
+  writeNumber(nums[4]);
+
+  delayMicroseconds(speedt);
+  
+  stopDigit(i);
+  stopDigit(i-1);
+  startDigit(i-2);
+  stopDigit(i-3);
+  stopDigit(i-4);
+  stopDigit(i-5);
+  writeNumber(nums[3]);
+
+  delayMicroseconds(speedt);
+
+  stopDigit(i);
+  stopDigit(i-1);
+  stopDigit(i-2);
+  startDigit(i-3);
+  stopDigit(i-4);
+  stopDigit(i-5);
+  writeNumber(nums[2]);
+
+  delayMicroseconds(speedt);
+
+  stopDigit(i);
+  stopDigit(i-1);
+  stopDigit(i-2);
+  stopDigit(i-3);
+  startDigit(i-4);
+  stopDigit(i-5);
+  writeNumber(nums[1]);
+
+  delayMicroseconds(speedt);
+
+  stopDigit(i);
+  stopDigit(i-1);
+  stopDigit(i-2);
+  stopDigit(i-3);
+  stopDigit(i-4);
+  startDigit(i-5);
+  writeNumber(nums[0]);
+
+  delayMicroseconds(speedt);
+  recvWithEndMarker();
+
     }
-  }
+  }  
 }
+
+
 
 boolean startProgram(){
   programStarted = true;
@@ -93,38 +154,44 @@ boolean startProgram(){
 
 void writeNumber(int whichNumber){
   for(byte i = 7; i >= 1;i--){
-    if(bitRead(arr[whichNumber], i)){
-      digitalWrite(DOT -i, LOW);
+    if(bitRead(numTable[whichNumber], i)){
+      digitalWrite(pins[7 -i], LOW);
     }
   }
 }
 
 void deleteNumber(){
-  for(int i =6; i<= 12; i++){
-    digitalWrite(i, HIGH);
+  for(int i =0; i<= 7; i++){
+    digitalWrite(pins[i], HIGH);
   }
 }
 
 void setupDigits(){
-  for(int i=2; i<=5;i++){
-    pinMode(i,OUTPUT);
-    digitalWrite(i, LOW);
+  for(int i=0; i<=3;i++){
+    pinMode(digits[i],OUTPUT);
+    digitalWrite(digits[i], LOW);
   }
 }
 
 void setupBars(){
-  for(int i = 6; i<=13; i++){
-    pinMode(i, OUTPUT);
-    digitalWrite(i, HIGH);
+  for(int i = 0; i<=8; i++){
+    pinMode(pins[i], OUTPUT);
+    digitalWrite(pins[i], HIGH);
   }
 }
 
-void startDigit(int digit){
-  digitalWrite(1+digit, HIGH);
+void startDigit(int num){
+  if(num <=3 && num >=0){
+     analogWrite(digits[num], brightns[num]);
+  }
 }
 
-void stopDigit(int digit){
-  digitalWrite(1+digit, LOW);
+void stopDigit(int num){
+  deleteNumber();
+  if(num <=3 && num >=0 ){
+    //digitalWrite(digits[num], LOW);
+    analogWrite(digits[num], 0);
+  }
 }
 
 boolean recvWithEndMarker() {
@@ -146,14 +213,38 @@ boolean recvWithEndMarker() {
     receivedChars[ndx] = '\0'; // terminate the string
     ndx = 0;
     newData = true;
+    if(receivedChars[0] == 'D'){
+      readSpeed();
+    } else if( receivedChars[0] == 'B'){
+      readBrghtns();
+    } else if (receivedChars[0] == 'T'){
+      setDigits();
+    }
     return true;
    }
  }
  return false;
 }
 
+void setDigits(){
+  for(int i =0; i<= 5; i++){
+    char b = receivedChars[i+1];
+    Serial.println(b);
+    if(b>=48 && b<=57){
+      nums[i] = b-48;
+      Serial.println(nums[i]);
+    } else if(b>=65 && b <=70){
+      nums[i] = b-55;
+      Serial.println(nums[i]);
+    }
+  }
+  if(newData){
+    newData = false;
+  }
+}
+
 void showNewData() {
- if (newData == true) {
+ if (newData) {
  Serial.print("This just in ... ");
  Serial.println(receivedChars);
  newData = false;
@@ -164,5 +255,38 @@ int parseData(){
    int numberRead = atoi(receivedChars);
    Serial.print("I read number: ");
    Serial.println(numberRead);
+   numberRight = numberRead;
    return numberRead;
 }
+
+void readBrghtns(){
+  for(int i =0; i<=3; i++){
+    char b = receivedChars[i+1];
+    switch(b){
+      case '0':
+        brightns[i] = dark;
+      break;
+      case '1':
+        brightns[i] = medium;
+        break;
+      case '2':
+        brightns[i] = high;
+       break;
+    }
+  }
+  if(newData){
+    newData = false;
+  }
+}
+
+void readSpeed(){
+
+  int numberRead = atoi(receivedChars+1);
+  Serial.print("SPEED: ");
+  Serial.println(numberRead);
+  speedt = numberRead;
+  if(newData){
+    newData = false;
+  }
+}
+
